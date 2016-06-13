97074cec397ca6b5692dc1be77743f80362880e3
glarson175 <gsl13@pitt.edu>
2016-06-12 20:36:26 -0400

    added tests

--- CityMapTester.java
+++ CityMapTester.java
@@ -9,9 +9,14 @@ public class CityMapTester {
 		CityMap map = new CityMap();
 		
 		assertEquals(true, map.getNeighbors("Hotel").contains("Library"));
+		assertEquals(true, map.getNeighbors("OC").contains("College"));
+		assertEquals(true, map.getNeighbors("College").contains("Diner"));
+		assertEquals(true, map.getNeighbors("Diner").contains("Hotel"));
+		assertEquals(true, map.getNeighbors("Library").contains("College"));
 		
 	}
 	
+	// tests to make sure all the locations are part of the map
 	@Test
 	public void getCitiesTest() {
 		CityMap map = new CityMap();
@@ -33,12 +38,17 @@ public class CityMapTester {
     	assertEquals(true, brandon.map.getNeighbors(brandon.curr).contains(brandon.nextLoc));
 		
 	}
+	
+	// Will test to make sure that the getStreet method works correctly
 	@Test
 	public void streetTest(){
 		CityMap Pitt = new CityMap();
-		System.out.print(Pitt.getStreet("OC", "College"));
+		//System.out.print(Pitt.getStreet("OC", "College"));
 		assertEquals("Fifth Avenue", Pitt.getStreet("OC", "College"));
+		assertEquals("Fourth Avenue", Pitt.getStreet("OC", "Hotel"));
 	}
-	}
+
+
+}
 
 
